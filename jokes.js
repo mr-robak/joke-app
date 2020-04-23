546a8bd08e3e8292537ca2fb18158d82220c3d0f
Marcin Robak <mr.robak@gmail.com>
2020-04-23 10:49:39 +0200

    added jokes to joke.js

---
+++ jokes.js
@@ -0,0 +1,13 @@
+const jokes = {
+  joke1: `What is the worst combination of two sicknesses?
+    Diarrhea and Alzheimer. You’re running, but you don’t know where.`,
+  joke2: `What is hairy, brown, and goes up and down?
+    -
+    A kiwi in an elevator.`,
+  joke3: `Chuck Norris doesn't pet any animals. Animals pet themselves when he approaches them.`,
+  joke4: `Recently, a police patrol stopped Chuck Norris when he was driving along. They got off with only a warning.`,
+
+  joke5: `Husband brings the child home from kindergarten and asks his   wife, "He’s been crying the whole way home. Isn’t he sick or something?"
+ 
+    "No," replies the wife, "he was just trying to tell you he isn’t our Frankie."`,
+};
